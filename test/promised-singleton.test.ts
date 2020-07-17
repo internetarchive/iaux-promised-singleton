1c2dbe79890ef62fee350320c073b6841868fba9
Jason Buckner <jason@jasonbuckner.com>
2020-07-17 09:36:57 -0700

    Add a Promise rejection test

--- test/promised-singleton.test.ts
+++ test/promised-singleton.test.ts
@@ -49,4 +49,43 @@ describe('Promised Singleton', () => {
       expect(results[index]).to.equal(firstResult);
     }
   });
+
+  // This test queues up 3 promises that get rejected and verifies that the same
+  // error message gets received by all three requests. This indicates they are
+  // all receiving the same rejection
+  it('rejects concurrent requests for the singleton', (done) => {
+    const promisedSingleton: PromisedSingleton<string> = new PromisedSingleton({
+      generator: new Promise((resolve, reject) => {
+        setTimeout(() => {
+          const random = Math.random();
+          reject(`ohno-${random}`);
+        }, 25);
+      }),
+    });
+
+    let firstError = '-';
+    promisedSingleton
+      .get()
+      .then(() => { expect.fail('should not get here'); })
+      .catch(error => {
+        firstError = error;
+    });
+
+    let secondError = '.';
+    promisedSingleton
+      .get()
+      .then(() => { expect.fail('should not get here'); })
+      .catch(error => {
+        secondError = error;
+      });
+
+    promisedSingleton
+      .get()
+      .then(() => { expect.fail('should not get here'); })
+      .catch(error => {
+        expect(error).to.equal(firstError);
+        expect(error).to.equal(secondError);
+        done()
+      });
+  });
 });
