682b2a4ebff0bc897d93a65909f09b471237ebc9
Jason Buckner <jason@jasonbuckner.com>
2020-07-17 09:44:25 -0700

    Linting

--- test/promised-singleton.test.ts
+++ test/promised-singleton.test.ts
@@ -37,7 +37,7 @@ describe('Promised Singleton', () => {
       generator: new Promise(resolve => {
         setTimeout(() => {
           const random = Math.random();
-          resolve(`foo-${random}`)
+          resolve(`foo-${random}`);
         }, 25);
       }),
     });
@@ -59,7 +59,7 @@ describe('Promised Singleton', () => {
   // This test queues up 3 promises that get rejected and verifies that the same
   // error message gets received by all three requests. This indicates they are
   // all receiving the same rejection
-  it('rejects concurrent requests for the singleton', (done) => {
+  it('rejects concurrent requests for the singleton', done => {
     const promisedSingleton: PromisedSingleton<string> = new PromisedSingleton({
       generator: new Promise((resolve, reject) => {
         setTimeout(() => {
@@ -72,26 +72,32 @@ describe('Promised Singleton', () => {
     let firstError = '-';
     promisedSingleton
       .get()
-      .then(() => { expect.fail('should not get here'); })
+      .then(() => {
+        expect.fail('should not get here');
+      })
       .catch(error => {
         firstError = error;
-    });
+      });
 
     let secondError = '.';
     promisedSingleton
       .get()
-      .then(() => { expect.fail('should not get here'); })
+      .then(() => {
+        expect.fail('should not get here');
+      })
       .catch(error => {
         secondError = error;
       });
 
     promisedSingleton
       .get()
-      .then(() => { expect.fail('should not get here'); })
+      .then(() => {
+        expect.fail('should not get here');
+      })
       .catch(error => {
         expect(error).to.equal(firstError);
         expect(error).to.equal(secondError);
-        done()
+        done();
       });
   });
 });
